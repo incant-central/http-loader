ab3e561bcfdebe9ff83b2dc7eac8ef9f2d45a823
Mac Heller-Ogden <mac@machellerogden.com>
2019-11-22 14:23:10 -0600

    WIP - refactor http loader

--- index.js
+++ index.js
@@ -2,63 +2,57 @@
 
 const axios = require('axios');
 
-function HttpLoader({ spec } = {}) {
+function HttpLoader({
+        spec,
+        input = {}
+    }) {
 
-    const {
-        url,
-        method = 'get',
-        headers = {},
-        params = {},
-        data = {},
-        timeout = 0,
-        withCredentials = false,
-        auth = {},
-        responseType = 'json',
-        responseEncoding = 'utf8',
-        xsrfCookieName = 'XSRF-TOKEN',
-        xsrfHeaderName = 'X-XSRF-TOKEN',
-        maxContentLength = 2000,
-        maxRedirects = 5,
-        socketPath = null,
-        proxy = null
-    } = spec;
+    const options = typeof spec === 'string'
+        ? {
+            url: spec
+          }
+        : spec;
 
-    async function httpTarget(requestOptions = {}, { name:cmdPath }) {
-        const {
-            data:requestData = {},
-            params:requestParams = {},
-        } = requestOptions;
+    async function httpTarget(io) {
 
-        const options = {
-            url,
-            method,
-            headers,
-            params: requestParams,
-            data: requestData,
-            timeout,
-            withCredentials,
-            auth,
-            responseType,
-            responseEncoding,
-            xsrfCookieName,
-            xsrfHeaderName,
-            maxContentLength,
-            maxRedirects,
-            socketPath,
-            proxy
-        };
+        if (typeof io === 'string') options.url = io;
 
-        const {
-            status,
-            statusText,
-            headers:responseHeaders,
-            data:responseData
-        } = await axios(options);
+        try {
+            const {
+                status,
+                statusText,
+                headers:responseHeaders,
+                data:responseData
+            } = await axios(options);
 
-        return responseData;
+            return responseData;
+        } catch (e) {
+            // TODO: handle
+            //throw e;
+        }
+
     }
 
     return httpTarget;
 }
 
 module.exports = HttpLoader;
+
+HttpLoader.study = () => [
+    'url',
+    'method',
+    'headers',
+    'params',
+    'data',
+    'timeout',
+    'withCredentials',
+    'auth',
+    'responseType',
+    'responseEncoding',
+    'xsrfCookieName',
+    'xsrfHeaderName',
+    'maxContentLength',
+    'maxRedirects',
+    'socketPath',
+    'proxy'
+];
